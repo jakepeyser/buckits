4dd8dd291a2ec04b1509482349c47149c276c191
Jake Peyser <jakepeyser@gmail.com>
2016-11-12 21:38:14 -0500

    goals return with liked status and routes for like/dislike goals - #9 #10

--- server/routes/goals.js
+++ server/routes/goals.js
@@ -3,7 +3,7 @@ const Goal = db.model('goal');
 const User = db.model('user');
 const router = require('express').Router();
 
-// --------------------> '/auth/' <-----------------------
+// --------------------> '/goals/' <-----------------------
 
 // Retrieve all goals
 router.get('/', (req, res, next) => {
@@ -19,6 +19,10 @@ router.get('/', (req, res, next) => {
   .then(goals => {
     // Calculate the likes and remove associated users
     goals.forEach(goal => {
+      // Check if the logged in user has liked this goal
+      goal.dataValues.liked =
+        goal.users.find(user => user.id === req.session.userId) !== undefined ? true : false;
+
       goal.dataValues.likes = goal.users.length;
       delete goal.dataValues.users;
     })
@@ -26,5 +30,49 @@ router.get('/', (req, res, next) => {
   })
   .catch(next);
 });
+
+// -------------------> '/goals/:goalId' <---------------------
+
+// Get the goal from the DB and place on req object
+router.param('goalId', (req, res, next, theGoalId) => {
+  Goal.findById(theGoalId)
+    .then(goal => {
+      req.goal = goal;
+      return next();
+    })
+    .catch(next);
+});
+
+// -----------------> '/goals/:goalId/like' <-------------------
+
+// Like a goal
+router.post('/:goalId/like', (req, res, next) => {
+  if (!req.session.userId) {
+    let error = new Error('You must be logged in to like a goal');
+    error.status = 401;
+    return next(error)
+  }
+
+  return User.findById(req.session.userId)
+  .then(user => user.addGoal(req.goal))
+  .then(() => res.sendStatus(201))
+  .catch(next);
+});
+
+// Unlike a goal
+router.delete('/:goalId/like', (req, res, next) => {
+  if (!req.session.userId) {
+    let error = new Error('You must be logged in to unlike a goal');
+    error.status = 401;
+    return next(error)
+  }
+
+  return User.findById(req.session.userId)
+  .then(user => user.removeGoal(req.goal))
+  .then(() => res.sendStatus(204))
+  .catch(next);
+});
+
+
 
 module.exports = router;
